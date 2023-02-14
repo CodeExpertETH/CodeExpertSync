b17b63a12e4223918387f2e2da865683256eaaa2
David Sichau <sichau@inf.ethz.ch>
2023-02-14 10:56:19 +0100

    remove folder which was accidential included

--- storybook-static/assets/chunk-GWAJ4KRU-d69fdc61.js
+++
@@ -1,1 +0,0 @@
-var e=t=>`control-${t.replace(/\s+/g,"-")}`,o=t=>`set-${t.replace(/\s+/g,"-")}`;export{e as a,o as g};
--- storybook-static/assets/preview-04419daf.js
+++
@@ -1,1 +0,0 @@
-const o={backgrounds:{default:"light"},actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}}};export{o as parameters};
