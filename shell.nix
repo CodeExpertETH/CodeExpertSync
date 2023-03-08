fa2992a39733cb58486683fa46eae09b70a9a7c6
Peter Gassner <peter@gassner.io>
2023-03-08 16:55:50 +0100

    fix: Upgrade Nix to use Apple SDK 11

--- shell.nix
+++ shell.nix
@@ -2,10 +2,11 @@ let
   pkgs = import (fetchTarball "https://channels.nixos.org/nixpkgs-unstable/nixexprs.tar.xz") { };
   nodejs = pkgs.nodejs-18_x;
   yarn = pkgs.yarn.override { inherit nodejs; };
+  apple_sdk = pkgs.darwin.apple_sdk_11_0;
   buildInputsDarwin = if pkgs.stdenv.isDarwin then [
-    pkgs.darwin.apple_sdk.frameworks.Carbon
-    pkgs.darwin.apple_sdk.frameworks.Cocoa
-    pkgs.darwin.apple_sdk.frameworks.WebKit
+    apple_sdk.frameworks.Carbon
+    apple_sdk.frameworks.Cocoa
+    apple_sdk.frameworks.WebKit
   ] else [ ];
 
 in pkgs.mkShell {
