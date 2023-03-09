564dfdab847ddf139ea1737fb154b3958b3111a7
Peter Gassner <peter@gassner.io>
2023-03-09 13:15:02 +0100

    fix: Prevent momentum scrolling on Webkit
    Without this, the main app can be overscrolled and shows the bouncing behaviour known from Webkit. While usually a nice feature, we don't want this in an app like ours, as it feels wobbly and unstable.

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from './App';
+import './global.css';
 import './reset.css';
 import './startup/registerApp.ts';
 
