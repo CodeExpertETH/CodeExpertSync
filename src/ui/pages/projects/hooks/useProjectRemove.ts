836ffea7b21c1420c22d65277361a07b31bfcd4f
David Sichau <sichau@inf.ethz.ch>
2023-04-18 10:33:07 +0200

    added missing dependency on hook

--- src/ui/pages/projects/hooks/useProjectRemove.ts
+++ src/ui/pages/projects/hooks/useProjectRemove.ts
@@ -5,19 +5,22 @@ import { ProjectId } from '../../../../domain/Project';
 import { createSignedAPIRequest } from '../../../../domain/createAPIRequest';
 
 export const useProjectRemove = (onProjectRemove: () => void) => {
-  const removeProject = React.useCallback((projectId: ProjectId) => {
-    void pipe(
-      createSignedAPIRequest({
-        path: 'app/projectAccess/remove',
-        method: 'POST',
-        payload: { projectId },
-        codec: iots.strict({ removed: iots.boolean }),
-      }),
-      //do here more code like removing the project from disk...
-      taskEither.map(onProjectRemove),
-      task.run,
-    );
-  }, []);
+  const removeProject = React.useCallback(
+    (projectId: ProjectId) => {
+      void pipe(
+        createSignedAPIRequest({
+          path: 'app/projectAccess/remove',
+          method: 'POST',
+          payload: { projectId },
+          codec: iots.strict({ removed: iots.boolean }),
+        }),
+        //do here more code like removing the project from disk...
+        taskEither.map(onProjectRemove),
+        task.run,
+      );
+    },
+    [onProjectRemove],
+  );
 
   return [removeProject] as const;
 };
