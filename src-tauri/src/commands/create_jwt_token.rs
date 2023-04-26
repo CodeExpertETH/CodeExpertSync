f9717ccf219ced3d9f9921512b9725c3fa22eb2e
Samuel Vogelsanger <samuel.vogelsanger@inf.ethz.ch>
2023-04-26 15:50:56 +0200

    polish: log useful error

--- src-tauri/src/commands/create_jwt_token.rs
+++ src-tauri/src/commands/create_jwt_token.rs
@@ -7,16 +7,16 @@ pub fn create_jwt_token(app_handle: tauri::AppHandle, claims: Value) -> Result<S
     app_handle
         .path_resolver()
         .app_local_data_dir()
-        .ok_or("Could not create JWT token".to_string())
+        .ok_or("Unable to determine app-local data dir".to_string())
         .map(|mut key_path| {
             key_path.push("privateKey.pem");
             key_path
         })
         .and_then(|key_path| {
-            fs::read(key_path).map_err(|_| "Could not read private key".to_string())
+            fs::read(key_path).map_err(|_| "Could not read private key file".to_string())
         })
         .and_then(|private_key| {
-            EncodingKey::from_ed_pem(&private_key).map_err(|_| "Invalid key found".to_string())
+            EncodingKey::from_ed_pem(&private_key).map_err(|_| "Invalid key file".to_string())
         })
         .and_then(|encoding_key| {
             encode(
@@ -24,7 +24,10 @@ pub fn create_jwt_token(app_handle: tauri::AppHandle, claims: Value) -> Result<S
                 &claims,
                 &encoding_key,
             )
-            .map_err(|_| "Could not create JWT token".to_string())
+            .map_err(|_| "Unable to encode JWT token".to_string())
         })
-        .map_err(|_| "Could not create JWT token".to_string()) // FIXME: this deletes all error - intentional?
+        .map_err(|e| {
+            eprintln!("Error: {e}");
+            "Could not create JWT token".to_string()
+        })
 }
