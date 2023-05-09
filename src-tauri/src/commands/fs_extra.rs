2abd1a32a84ec98115414d215addaf779d63fef7
David Sichau <sichau@inf.ethz.ch>
2023-05-09 08:49:46 +0200

    optimized rust code

--- src-tauri/src/commands/fs_extra.rs
+++ src-tauri/src/commands/fs_extra.rs
@@ -1,44 +1,35 @@
 use std::fs;
-use std::fs::File;
+use std::fs::{metadata, File};
 use std::path::Path;
 
 #[tauri::command]
 pub fn make_readonly(path: String) -> Result<(), String> {
-    let f = File::open(path).map_err(|e| format!("Could not open file: {e}"))?;
-
-    let metadata = f
-        .metadata()
-        .map_err(|e| format!("Could not read metadata of file: {e}"))?;
-    let mut permissions = metadata.permissions();
-    permissions.set_readonly(true);
-    f.set_permissions(permissions)
-        .map_err(|e| format!("Could not set permissions of file: {e}"))?;
-
-    Ok(())
+    let binding = Path::new(&path);
+    set_read_only(binding)
 }
 
 fn set_read_only(path: &Path) -> Result<(), String> {
-    let f = File::open(path).map_err(|e| format!("Could not open file: {e}"))?;
-    let metadata = f
-        .metadata()
-        .map_err(|e| format!("Could not read metadata of file: {e}"))?;
-    let mut permissions = metadata.permissions();
-    permissions.set_readonly(true);
-    f.set_permissions(permissions)
-        .map_err(|e| format!("Could not set permissions of file: {e}"))?;
-    Ok(())
+    File::open(path)
+        .and_then(|f| {
+            f.metadata().map(|m| m.permissions()).map(|mut p| {
+                p.set_readonly(true);
+                f.set_permissions(p)
+            })
+        })
+        .map(|_| ())
+        .map_err(|e| format!("Could not set permissions of file: {e}"))
 }
 
 fn remove_read_only(path: &Path) -> Result<(), String> {
-    let f = File::open(path).map_err(|e| format!("Could not open file: {e}"))?;
-    let metadata = f
-        .metadata()
-        .map_err(|e| format!("Could not read metadata of file: {e}"))?;
-    let mut permissions = metadata.permissions();
-    permissions.set_readonly(false);
-    f.set_permissions(permissions)
-        .map_err(|e| format!("Could not set permissions of file: {e}"))?;
-    Ok(())
+    File::open(path)
+        .and_then(|f| {
+            f.metadata().map(|m| m.permissions()).map(|mut p| {
+                p.set_readonly(false);
+                f.set_permissions(p)
+            })
+        })
+        .map(|_| ())
+        .map_err(|e| format!("Could not set permissions of file: {e}"))
 }
 
 #[tauri::command]
@@ -47,14 +38,14 @@ pub fn create_project_dir(path: String) -> Result<(), String> {
     let mut ancestors = binding.ancestors();
     let mut ancestors2 = binding.ancestors();
     // loop over ancestors until path exists or root is reached
-    while let Some(p) = ancestors.next() {
+    for p in &mut ancestors {
         if p.exists() && p.is_dir() {
             //remove readonly from path
-            remove_read_only(&p)?;
+            remove_read_only(p)?;
             // create dir
-            fs::create_dir_all(&binding).map_err(|e| format!("Could not create dir: {e}"))?;
+            fs::create_dir_all(binding).map_err(|e| format!("Could not create dir: {e}"))?;
             // set permissions back to readonly for whole path
-            while let Some(p2) = ancestors2.next() {
+            for p2 in &mut ancestors2 {
                 set_read_only(p2)?;
                 if p2 == p {
                     break;
