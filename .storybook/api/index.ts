04c6f065458666d108963131f5caad8dbbfc4f37
Peter Gassner <peter@gassner.io>
2023-05-12 14:02:12 +0200

    fix: Make Storybook work again

--- .storybook/api/index.ts
+++ .storybook/api/index.ts
@@ -1,14 +1,38 @@
-import { taskOption } from '../../src/prelude';
+import { either, option, pipe, task, taskEither, taskOption } from '@code-expert/prelude';
 import type { Api } from '../../src/api';
 
-const store = new Map();
+const configStore = new Map();
+const fileStore = new Map();
+const settingsStore = new Map();
 
 export const api: Api = {
-  getVersion: () => Promise.resolve('Version: Storybook'),
-  greet: (name) => Promise.resolve(`Hello from Storybook, ${name}!`),
-  settingRead: (key) => taskOption.fromIO(() => store.get(key)),
+  getVersion: () => () => Promise.resolve(either.right('Version: Storybook')),
+  create_keys: () => taskEither.of('FIXME secret-key'),
+  create_jwt_tokens: () => taskEither.of('FIXME jwt-token'),
+  settingRead: (key) => taskOption.fromIO(() => settingsStore.get(key)),
   settingWrite: (key, value) => () => {
-    store.set(key, value);
-    return Promise.resolve(undefined);
+    settingsStore.set(key, value);
+    return Promise.resolve(option.some(undefined));
   },
+  writeConfigFile: (name, value) =>
+    taskEither.fromIO(() => {
+      configStore.set(name, value);
+    }),
+  writeFile: (filePath, content) =>
+    taskEither.fromIO(() => {
+      fileStore.set(filePath, content);
+    }),
+  removeDir: () => {
+    throw new Error('[Storybook] Not implemented');
+  },
+  getFileHash: () => {
+    throw new Error('[Storybook] Not implemented');
+  },
+  createProjectDir: () => {
+    throw new Error('[Storybook] Not implemented');
+  },
+  readConfigFile: (name, decoder) =>
+    pipe(configStore.get(name), decoder.decode, taskOption.fromEither),
+  hasConfigFile: (name) => task.fromIO(() => configStore.get(name) != null),
+  logout: () => taskOption.of(undefined),
 };
