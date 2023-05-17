ee308b1994dd0b519cfd1a1cfd6ee8bd973c23f3
Peter Gassner <peter@gassner.io>
2023-05-17 08:22:34 +0200

    feat: Re-add useRemoteData hook

---
+++ src/ui/hooks/useRemoteData.stories.tsx
@@ -0,0 +1,100 @@
+// ui/hooks/useRemoteData.stories.tsx
+import { Story } from '@storybook/react';
+import React from 'react';
+import { adt, pipe, remoteData, task } from '@code-expert/prelude';
+import { UncaughtException } from '@/domain/exception';
+import { useCachedRemoteData, useRemoteData } from './useRemoteData';
+
+const foldMeteorCallType = adt.foldFromKeys({ resolve: null, reject: null, throw: null });
+type MeteorCallType = adt.TypeOfKeys<typeof foldMeteorCallType>;
+
+function mockMethodCall(type: MeteorCallType): task.Task<string> {
+  return () =>
+    new Promise((resolve, reject) => {
+      foldMeteorCallType(type, {
+        resolve: () => {
+          setTimeout(() => resolve(`resolve ${Math.random()}`), 1000);
+        },
+        reject: () => {
+          setTimeout(() => reject(new UncaughtException(`reject ${Math.random()}`)), 1000);
+        },
+        throw: () => {
+          throw new UncaughtException(`throw ${Math.random()}`);
+        },
+      });
+    });
+}
+
+// -----------------------------------------------------------------------------
+
+interface UseRemoteDataTestProps {
+  type: MeteorCallType;
+  useRemoteDataHook: typeof useRemoteData;
+}
+
+function UseRemoteDataTest({ type, useRemoteDataHook }: UseRemoteDataTestProps) {
+  const [rd, refresh] = useRemoteDataHook(mockMethodCall);
+  return (
+    <div>
+      {pipe(
+        rd,
+        remoteData.fold(
+          () => <div>[Initial]</div>,
+          () => <div>[Loading]</div>,
+          (e) => <div>[Error] {e.message}</div>,
+          (x) => <div>[Success] {x}</div>,
+        ),
+      )}
+      <button type="button" onClick={() => refresh(type)}>
+        {remoteData.isInitial(rd) || remoteData.isPending(rd) ? 'Fetch' : 'Refresh'}
+      </button>
+    </div>
+  );
+}
+
+// -----------------------------------------------------------------------------
+
+export default {
+  title: 'hooks/useRemoteData',
+  component: UseRemoteDataTest,
+};
+
+const Template: Story<{ type: MeteorCallType; mode: 'fresh' | 'stale' }> = ({
+  mode = 'fresh',
+  type = 'resolve',
+}) => (
+  <UseRemoteDataTest
+    type={type}
+    useRemoteDataHook={mode === 'fresh' ? useRemoteData : useCachedRemoteData}
+  />
+);
+
+export const Resolve = Template.bind({});
+Resolve.args = {};
+
+export const Reject = Template.bind({});
+Reject.args = {
+  type: 'reject',
+};
+
+export const Throw = Template.bind({});
+Throw.args = {
+  type: 'throw',
+};
+
+export const CachedResolve = Template.bind({});
+CachedResolve.args = {
+  mode: 'stale',
+};
+
+export const CachedReject = Template.bind({});
+CachedReject.args = {
+  mode: 'stale',
+  type: 'reject',
+};
+
+export const CachedThrow = Template.bind({});
+CachedThrow.args = {
+  mode: 'stale',
+  type: 'throw',
+};
---
+++ src/ui/hooks/useRemoteData.ts
@@ -0,0 +1,60 @@
+import React from 'react';
+import { flow, remoteData, task } from '@code-expert/prelude';
+import { Exception, fromError } from '@/domain/exception';
+import { useRaceState } from './useRaceState';
+
+/**
+ * Run a `Task` and represent the states before, during and after as `RemoteData`.
+ */
+export function useRemoteData<P, A>(run: (props: P) => task.Task<A>) {
+  const [state, mkSetState] = useRaceState<remoteData.RemoteData<Exception, A>>(remoteData.initial);
+
+  const { current } = React.useRef({
+    run,
+    refresh(props: P) {
+      const setState = mkSetState();
+      setState(remoteData.pending);
+      void (async () => {
+        try {
+          const result: A = await task.run(current.run(props));
+          setState(remoteData.success(result));
+        } catch (e) {
+          setState(remoteData.failure(fromError(e)));
+        }
+      })();
+    },
+  });
+
+  current.run = run;
+
+  return [state, current.refresh] as const;
+}
+
+/**
+ * Run a `Task` and represent the states before, during and after as `RemoteData`. When refreshing,
+ * keep returning stale data until new results are available, according to Stale-While-Revalidate.
+ */
+export function useCachedRemoteData<P, A>(run: (props: P) => task.Task<A>) {
+  const [state, mkSetState] = useRaceState<remoteData.RemoteData<Exception, A>>(remoteData.initial);
+  const strategy = remoteData.staleWhileRevalidate<Exception, A>;
+
+  const { current } = React.useRef({
+    run,
+    refresh(props: P) {
+      const setState = flow(strategy, mkSetState());
+      setState(remoteData.pending);
+      void (async () => {
+        try {
+          const result: A = await task.run(current.run(props));
+          setState(remoteData.success(result));
+        } catch (e) {
+          setState(remoteData.failure(fromError(e)));
+        }
+      })();
+    },
+  });
+
+  current.run = run;
+
+  return [state, current.refresh] as const;
+}
