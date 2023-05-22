df7736760835a9c3386459d44bed745d07f16085
Peter Gassner <peter@gassner.io>
2023-05-22 17:39:54 +0200

    feat: Make Storybook preview config type-safe

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,12 +1,23 @@
-export const parameters = {
-  backgrounds: {
-    default: 'light',
-  },
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
+import { Preview } from '@storybook/react';
+import React from 'react';
+import { GlobalContextProvider } from '../src/ui/GlobalContext';
+
+const preview: Preview = {
+  parameters: {
+    backgrounds: {
+      default: 'light',
+    },
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
     },
   },
-}
+  decorators: [
+    (Story) => React.createElement(GlobalContextProvider, {}, React.createElement(Story)),
+  ],
+};
+
+export default preview;
