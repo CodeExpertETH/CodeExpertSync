4086c19a85fe46287ca8682342ccfe6f9607b156
Samuel Vogelsanger <samuel.vogelsanger@inf.ethz.ch>
2023-07-06 15:24:30 +0200

    refact: replace deprecated

--- packages/prelude/eithert.ts
+++ packages/prelude/eithert.ts
@@ -1,11 +1,9 @@
 import { apply, functor, monad, option, pipeable, pointed, predicate } from 'fp-ts';
 import { Applicative1, Applicative2 } from 'fp-ts/Applicative';
 import * as eitherT from 'fp-ts/EitherT';
-import { EitherT1, EitherT2 } from 'fp-ts/EitherT';
 import { Endomorphism } from 'fp-ts/Endomorphism';
 import { Kind, Kind2, URIS, URIS2 } from 'fp-ts/HKT';
 import { FunctionN, Lazy, flow, pipe, tupled } from 'fp-ts/function';
-
 import * as either from './either';
 
 export * from 'fp-ts/EitherT';
@@ -17,21 +15,21 @@ type Sequence2 = {
    * Tuple sequencing; executes two monadic computations and combines the results as a tuple.
    */
   <F extends URIS2>(F: Applicative2<F>): <FE, E, A, B>(
-    a: EitherT2<F, FE, E, A>,
-    b: EitherT2<F, FE, E, B>,
-  ) => EitherT2<F, FE, E, [A, B]>;
+    a: Kind2<F, FE, either.Either<E, A>>,
+    b: Kind2<F, FE, either.Either<E, B>>,
+  ) => Kind2<F, FE, either.Either<E, [A, B]>>;
   /**
    * Tuple sequencing; executes two monadic computations and combines the results as a tuple.
    */
   <F extends URIS>(F: Applicative1<F>): <E, A, B>(
-    a: EitherT1<F, E, A>,
-    b: EitherT1<F, E, B>,
-  ) => EitherT1<F, E, [A, B]>;
+    a: Kind<F, either.Either<E, A>>,
+    b: Kind<F, either.Either<E, B>>,
+  ) => Kind<F, either.Either<E, [A, B]>>;
 };
 
 export const sequence2: Sequence2 =
   <F extends URIS>(F: Applicative1<F>) =>
-  <E, A, B>(a: EitherT1<F, E, A>, b: EitherT1<F, E, B>) => {
+  <E, A, B>(a: Kind<F, either.Either<E, A>>, b: Kind<F, either.Either<E, B>>) => {
     const FS = pipeable.pipeable(F);
     const seq: (ea: either.Either<E, A>) => (eb: either.Either<E, B>) => either.Either<E, [A, B]> =
       (ea) => (eb) =>
@@ -46,29 +44,29 @@ type Map2 = {
    * Executes two monadic computations and applies a function to the result.
    */
   <F extends URIS2>(F: Applicative2<F>): <FE, E, A, B>(
-    a: EitherT2<F, FE, E, A>,
-    b: EitherT2<F, FE, E, B>,
-  ) => <R>(f: FunctionN<[A, B], R>) => EitherT2<F, FE, E, R>;
+    a: Kind2<F, FE, either.Either<E, A>>,
+    b: Kind2<F, FE, either.Either<E, B>>,
+  ) => <R>(f: FunctionN<[A, B], R>) => Kind2<F, FE, either.Either<E, R>>;
   /**
    * Executes two monadic computations and applies a function to the result.
    */
   <F extends URIS>(F: Applicative1<F>): <E, A, B>(
-    a: EitherT1<F, E, A>,
-    b: EitherT1<F, E, B>,
-  ) => <R>(f: FunctionN<[A, B], R>) => EitherT1<F, E, R>;
+    a: Kind<F, either.Either<E, A>>,
+    b: Kind<F, either.Either<E, B>>,
+  ) => <R>(f: FunctionN<[A, B], R>) => Kind<F, either.Either<E, R>>;
 };
 
 export const map2: Map2 =
   <F extends URIS>(F: Applicative1<F>) =>
-  <E, A, B>(a: EitherT1<F, E, A>, b: EitherT1<F, E, B>) =>
+  <E, A, B>(a: Kind<F, either.Either<E, A>>, b: Kind<F, either.Either<E, B>>) =>
   <R>(f: FunctionN<[A, B], R>) =>
     pipe(sequence2(F)(a, b), eitherT.map(F)(tupled(f)));
 
 // –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
 
 type Of = {
-  <F extends URIS2, FE>(F: pointed.Pointed2<F>): <E, A>(a: A) => EitherT2<F, FE, E, A>;
-  <F extends URIS>(F: pointed.Pointed1<F>): <E, A>(a: A) => EitherT1<F, E, A>;
+  <F extends URIS2, FE>(F: pointed.Pointed2<F>): <E, A>(a: A) => Kind2<F, FE, either.Either<E, A>>;
+  <F extends URIS>(F: pointed.Pointed1<F>): <E, A>(a: A) => Kind<F, either.Either<E, A>>;
 };
 
 export const of: Of = <F extends URIS>(F: pointed.Pointed1<F>) => eitherT.right(F);
@@ -82,19 +80,19 @@ type FilterOrElse = {
   <F extends URIS2>(F: functor.Functor2<F>): <FE, E, A>(
     p: predicate.Predicate<A>,
     onFalse: Lazy<E>,
-  ) => Endomorphism<EitherT2<F, FE, E, A>>;
+  ) => Endomorphism<Kind2<F, FE, either.Either<E, A>>>;
   /**
    * Filters a value based on a predicate.
    */
   <F extends URIS>(F: functor.Functor1<F>): <E, A>(
     p: predicate.Predicate<A>,
     onFalse: Lazy<E>,
-  ) => Endomorphism<EitherT1<F, E, A>>;
+  ) => Endomorphism<Kind<F, either.Either<E, A>>>;
 };
 
 export const filterOrElse: FilterOrElse =
   <F extends URIS>(F: functor.Functor1<F>) =>
-  <E, A>(p: predicate.Predicate<A>, onFalse: Lazy<E>): Endomorphism<EitherT1<F, E, A>> =>
+  <E, A>(p: predicate.Predicate<A>, onFalse: Lazy<E>): Endomorphism<Kind<F, either.Either<E, A>>> =>
     pipeable.pipeable(F).map(either.filterOrElse(p, onFalse));
 
 // –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
@@ -106,14 +104,14 @@ type FilterOrElseM = {
   <F extends URIS2>(F: monad.Monad2<F>): <FE, E, A>(
     p: FunctionN<[A], Kind2<F, FE, boolean>>,
     onFalse: Lazy<E>,
-  ) => Endomorphism<EitherT2<F, FE, E, A>>;
+  ) => Endomorphism<Kind2<F, FE, either.Either<E, A>>>;
   /**
    * Filters a value based on a monadic predicate.
    */
   <F extends URIS>(F: monad.Monad1<F>): <E, A>(
     p: FunctionN<[A], Kind<F, boolean>>,
     onFalse: Lazy<E>,
-  ) => Endomorphism<EitherT1<F, E, A>>;
+  ) => Endomorphism<Kind<F, either.Either<E, A>>>;
 };
 
 export const filterOrElseM: FilterOrElseM =
@@ -121,10 +119,10 @@ export const filterOrElseM: FilterOrElseM =
   <E, A>(
     p: FunctionN<[A], Kind<F, boolean>>,
     onFalse: Lazy<E>,
-  ): Endomorphism<EitherT1<F, E, A>> => {
+  ): Endomorphism<Kind<F, either.Either<E, A>>> => {
     const FF = pipeable.pipeable(F);
     return FF.chain(
-      either.fold<E, A, EitherT1<F, E, A>>(eitherT.left(F), (a) =>
+      either.fold<E, A, Kind<F, either.Either<E, A>>>(eitherT.left(F), (a) =>
         pipe(p(a), FF.map(either.fromBoolean(onFalse, () => a))),
       ),
     );
@@ -136,16 +134,16 @@ type FromPredicate = {
   <F extends URIS2>(F: pointed.Pointed2<F>): <E, A>(
     p: predicate.Predicate<A>,
     onFalse: Lazy<E>,
-  ) => <FE>(_: A) => EitherT2<F, FE, E, A>;
+  ) => <FE>(_: A) => Kind2<F, FE, either.Either<E, A>>;
   <F extends URIS>(F: pointed.Pointed1<F>): <E, A>(
     p: predicate.Predicate<A>,
     onFalse: Lazy<E>,
-  ) => (_: A) => EitherT1<F, E, A>;
+  ) => (_: A) => Kind<F, either.Either<E, A>>;
 };
 
 export const fromPredicate: FromPredicate =
   <F extends URIS>(F: pointed.Pointed1<F>) =>
-  <E, A>(p: predicate.Predicate<A>, onFalse: Lazy<E>): ((_: A) => EitherT1<F, E, A>) =>
+  <E, A>(p: predicate.Predicate<A>, onFalse: Lazy<E>): ((_: A) => Kind<F, either.Either<E, A>>) =>
     flow(either.fromPredicate(p, onFalse), (e) => F.of(e));
 
 // –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
@@ -155,20 +153,22 @@ type ChainFirst = {
    * Chain operation which discards the resulting value and returns the original value.
    */
   <F extends URIS2>(F: monad.Monad2<F>): <FE, E, A, B>(
-    _: FunctionN<[A], EitherT2<F, FE, E, B>>,
-  ) => Endomorphism<EitherT2<F, FE, E, A>>;
+    f: (a: A) => Kind2<F, FE, either.Either<E, B>>,
+  ) => Endomorphism<Kind2<F, FE, either.Either<E, A>>>;
   /**
    * Chain operation which discards the resulting value and returns the original value.
    */
   <F extends URIS>(F: monad.Monad1<F>): <E, A, B>(
-    _: FunctionN<[A], EitherT1<F, E, B>>,
-  ) => Endomorphism<EitherT1<F, E, A>>;
+    f: (a: A) => Kind<F, either.Either<E, B>>,
+  ) => Endomorphism<Kind<F, either.Either<E, B>>>;
 };
 
 export const chainFirst: ChainFirst =
   <F extends URIS>(
     F: monad.Monad1<F>,
-  ): (<E, A, B>(_: FunctionN<[A], EitherT1<F, E, B>>) => Endomorphism<EitherT1<F, E, A>>) =>
+  ): (<E, A, B>(
+    f: (a: A) => Kind<F, either.Either<E, B>>,
+  ) => Endomorphism<Kind<F, either.Either<E, A>>>) =>
   (f) =>
     eitherT.chain(F)((a) =>
       pipe(
@@ -190,7 +190,7 @@ type SemiChain = {
    */
   <F extends URIS2>(F: monad.Monad2<F>): <FE, E, A, B>(
     _: FunctionN<[A], Kind2<F, FE, B>>,
-  ) => FunctionN<[EitherT2<F, FE, E, A>], EitherT2<F, FE, E, B>>;
+  ) => FunctionN<[Kind2<F, FE, either.Either<E, A>>], Kind2<F, FE, either.Either<E, B>>>;
   /**
    * Chain operation over the transformed (outer) monad.
    * @example
@@ -201,7 +201,7 @@ type SemiChain = {
    */
   <F extends URIS>(F: monad.Monad1<F>): <E, A, B>(
     _: FunctionN<[A], Kind<F, B>>,
-  ) => FunctionN<[EitherT1<F, E, A>], EitherT1<F, E, B>>;
+  ) => FunctionN<[Kind<F, either.Either<E, A>>], Kind<F, either.Either<E, B>>>;
 };
 
 export const semiChain: SemiChain =
@@ -209,7 +209,7 @@ export const semiChain: SemiChain =
     F: monad.Monad1<F>,
   ): (<E, A, B>(
     _: FunctionN<[A], Kind<F, B>>,
-  ) => FunctionN<[EitherT1<F, E, A>], EitherT1<F, E, B>>) =>
+  ) => FunctionN<[Kind<F, either.Either<E, A>>], Kind<F, either.Either<E, B>>>) =>
   (f) =>
     eitherT.chain(F)(
       flow(
@@ -232,7 +232,7 @@ type SemiChainFirst = {
    */
   <F extends URIS2>(F: monad.Monad2<F>): <FE, E, A, B>(
     _: FunctionN<[A], Kind2<F, FE, B>>,
-  ) => Endomorphism<EitherT2<F, FE, E, A>>;
+  ) => Endomorphism<Kind2<F, FE, either.Either<E, A>>>;
   /**
    * Chain operation over the transformed (outer) monad which discards the resulting value and returns
    * the original value.
@@ -244,13 +244,13 @@ type SemiChainFirst = {
    */
   <F extends URIS>(F: monad.Monad1<F>): <E, A, B>(
     _: FunctionN<[A], Kind<F, B>>,
-  ) => Endomorphism<EitherT1<F, E, A>>;
+  ) => Endomorphism<Kind<F, either.Either<E, A>>>;
 };
 
 export const semiChainFirst: SemiChainFirst =
   <F extends URIS>(
     F: monad.Monad1<F>,
-  ): (<E, A, B>(_: FunctionN<[A], Kind<F, B>>) => Endomorphism<EitherT1<F, E, A>>) =>
+  ): (<E, A, B>(_: FunctionN<[A], Kind<F, B>>) => Endomorphism<Kind<F, either.Either<E, A>>>) =>
   (f) =>
     semiChain(F)((a) =>
       pipe(
@@ -268,14 +268,14 @@ type FromOptionF = {
    */
   <F extends URIS2>(F: functor.Functor2<F>): <FE, E>(
     onNone: Lazy<E>,
-  ) => <A>(_: Kind2<F, FE, option.Option<A>>) => EitherT2<F, FE, E, A>;
+  ) => <A>(_: Kind2<F, FE, option.Option<A>>) => Kind2<F, FE, either.Either<E, A>>;
   /**
    * Create an F of an either based on an F of an option.
    * Lazy<E> => F<Option<A>> => F<Either<E, A>>
    */
   <F extends URIS>(F: functor.Functor1<F>): <E>(
     onNone: Lazy<E>,
-  ) => <A>(_: Kind<F, option.Option<A>>) => EitherT1<F, E, A>;
+  ) => <A>(_: Kind<F, option.Option<A>>) => Kind<F, either.Either<E, A>>;
 };
 
 export const fromOptionF: FromOptionF =
@@ -293,7 +293,7 @@ type FromBooleanF = {
   <F extends URIS2>(F: functor.Functor2<F>): <FE, E, A>(
     onFalse: Lazy<E>,
     onTrue: Lazy<A>,
-  ) => (_: Kind2<F, FE, boolean>) => EitherT2<F, FE, E, A>;
+  ) => (_: Kind2<F, FE, boolean>) => Kind2<F, FE, either.Either<E, A>>;
   /**
    * Create an F of an either based on an F of a condition.
    * (Lazy<E>, Lazy<A>) => F<boolean> => F<Either<E, A>>
@@ -301,7 +301,7 @@ type FromBooleanF = {
   <F extends URIS>(F: functor.Functor1<F>): <E, A>(
     onFalse: Lazy<E>,
     onTrue: Lazy<A>,
-  ) => <A>(_: Kind<F, boolean>) => EitherT1<F, E, A>;
+  ) => <A>(_: Kind<F, boolean>) => Kind<F, either.Either<E, A>>;
 };
 
 export const fromBooleanF: FromBooleanF =
