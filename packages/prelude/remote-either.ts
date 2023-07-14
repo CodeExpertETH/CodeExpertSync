201503ef6af0283cbc172f409840371f373d4bd2
Samuel Vogelsanger <samuel.vogelsanger@inf.ethz.ch>
2023-07-14 10:27:19 +0200

    refact: move files around

--- packages/prelude/remote-either.ts
+++ packages/prelude/remote-either.ts
@@ -1,9 +1,6 @@
-import { remote, remoteEither } from '@code-expert/remote-ts';
-import { $IntentionalAny } from '@code-expert/type-utils';
+import { remote, remoteEither } from '@code-expert/fp-ts-remote';
 import * as eT from './eithert';
 
 module.exports = remoteEither;
-
-export declare const foo: $IntentionalAny;
 
 export const fromOption = eT.fromOptionF(remote.Functor);
