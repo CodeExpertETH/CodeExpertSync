25a897934dd108070e6ae5efa548c02e82d2d162
Peter Gassner <peter@gassner.io>
2023-07-25 07:35:10 +0200

    polish: Co-locate deleteSingleFile with domain/FsNode
    It works on an FsFile, so it's a better fit there

--- src/domain/FileSystem/FsNode.ts
+++ src/domain/FileSystem/FsNode.ts
@@ -1,4 +1,5 @@
-import { iots } from '@code-expert/prelude';
+import { constVoid, iots, pipe, task } from '@code-expert/prelude';
+import { FileSystemStack } from '@/domain/FileSystem/fileSystemStack';
 import { FsDir, FsFile } from '@/lib/tauri/fs';
 
 // "export as" with identical name is necessary for auto-imports to list this file as possible source
@@ -13,3 +14,10 @@ export const FsFileC: iots.Type<FsFile> = iots.strict({
   type: iots.literal('file'),
   path: iots.string,
 });
+
+// -------------------------------------------------------------------------------------------------
+
+export const deleteSingleFile =
+  (stack: FileSystemStack, projectDir: string) =>
+  (file: FsFile): task.Task<void> =>
+    pipe(stack.join(projectDir, file.path), task.chainFirst(stack.removeFile), task.map(constVoid));
--- src/domain/FileSystem/LocalFileInfo.ts
+++ src/domain/FileSystem/LocalFileInfo.ts
@@ -1,14 +1,6 @@
-import { constVoid, iots, pipe, task } from '@code-expert/prelude';
+import { iots } from '@code-expert/prelude';
 import { FsFile, FsFileC } from './FsNode';
 import { HashInfo, HashInfoC } from './HashInfo';
-import { FileSystemStack } from './fileSystemStack';
 
 export interface LocalFileInfo extends FsFile, HashInfo {}
 export const LocalFileInfoC: iots.Type<LocalFileInfo> = iots.intersection([FsFileC, HashInfoC]);
-
-// -------------------------------------------------------------------------------------------------
-
-export const deleteSingleFile =
-  (stack: FileSystemStack, projectDir: string) =>
-  (file: FsFile): task.Task<void> =>
-    pipe(stack.join(projectDir, file.path), task.chainFirst(stack.removeFile), task.map(constVoid));
