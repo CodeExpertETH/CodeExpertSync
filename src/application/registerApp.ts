d90c2c22f4c873b77adacffdbdbce91b09388156
Peter Gassner <peter@gassner.io>
2023-08-18 18:40:22 +0200

    feat: Remove registration token step

--- src/application/registerApp.ts
+++ src/application/registerApp.ts
@@ -2,7 +2,7 @@ import { getName, getVersion } from '@tauri-apps/api/app';
 import { api } from 'api';
 import { flow, iots, pipe, task, taskEither, taskOption } from '@code-expert/prelude';
 import { ClientId } from '@/domain/ClientId';
-import { apiErrorToMessage, apiGet, apiPost, requestBody } from '@/utils/api';
+import { apiErrorToMessage, apiPost, requestBody } from '@/utils/api';
 import { panic } from '@/utils/error';
 
 const getSystemInfo: task.Task<{ os: string; name: string; version: string }> = task.sequenceS({
@@ -14,30 +14,18 @@ const getSystemInfo: task.Task<{ os: string; name: string; version: string }> = 
   version: getVersion,
 });
 
-const getRegistrationToken: task.Task<string> = pipe(
-  apiGet({
-    path: 'access/register',
-    codec: iots.strict({ token: iots.string }),
-  }),
-  taskEither.getOrElse(flow(apiErrorToMessage, panic)),
-  task.map(({ token }) => token),
-);
-
 export const registerApp = (): task.Task<ClientId> =>
   pipe(
     api.settingRead('clientId', ClientId),
     taskOption.getOrElse(() =>
       pipe(
-        task.Do,
-        task.bind('systemInfo', () => getSystemInfo),
-        task.bind('token', () => getRegistrationToken),
-        task.chain(({ systemInfo, token }) =>
+        getSystemInfo,
+        task.chain((systemInfo) =>
           pipe(
             apiPost({
               path: 'access/register',
               body: requestBody.json({
                 ...systemInfo,
-                token,
                 permissions: ['user:read', 'project:read', 'project:write'],
               }),
               codec: iots.strict({ clientId: ClientId }),
