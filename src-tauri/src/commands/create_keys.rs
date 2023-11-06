71b6cc2c63bac2bc8f398497583bb4272a04a364
David Sichau <sichau@inf.ethz.ch>
2023-11-06 13:28:40 +0100

    ensure that app local dir exists

--- src-tauri/src/commands/create_keys.rs
+++ src-tauri/src/commands/create_keys.rs
@@ -9,13 +9,17 @@ pub fn create_keys(app_handle: tauri::AppHandle) -> Result<String, String> {
         .path_resolver()
         .app_local_data_dir()
         .ok_or("Did not find local dir".to_string())
-        .map(|mut key_path| {
-            key_path.push("privateKey.pem");
-            key_path
-        })
-        .and_then(|key_path| {
-            fs::write(key_path, kp.sk.to_pem())
-                .map_err(|_| "Could not write private key".to_string())
+        .map(|mut path| {
+            fs::create_dir_all(&path)
+                .map_err(|_| "Could not create app local data dir".to_string())
+                .map(|_| {
+                    path.push("privateKey.pem");
+                    path
+                })
+                .and_then(|key_path| {
+                    fs::write(key_path, kp.sk.to_pem())
+                        .map_err(|_| "Could not write private key".to_string())
+                })
         })
         .map(|_| kp.pk.to_pem())
 }
